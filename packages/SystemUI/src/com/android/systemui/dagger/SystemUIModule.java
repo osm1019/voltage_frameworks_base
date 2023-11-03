f60773da5919e85473ecff07d156e50b08d6283f
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2023-11-03 09:00:09 +0000

    base: Init VoltageOS QS Module
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -80,6 +80,7 @@ import com.android.systemui.qrcodescanner.dagger.QRCodeScannerModule;
 import com.android.systemui.qs.FgsManagerController;
 import com.android.systemui.qs.FgsManagerControllerImpl;
 import com.android.systemui.qs.QSFragmentStartableModule;
+import com.android.systemui.qs.dagger.voltage.VoltageModule;
 import com.android.systemui.qs.footer.dagger.FooterActionsModule;
 import com.android.systemui.recents.Recents;
 import com.android.systemui.recordissue.RecordIssueModule;
@@ -189,6 +190,7 @@ import javax.inject.Named;
         DisplayModule.class,
         DreamModule.class,
         EventLogModule.class,
+        VoltageModule.class,
         FalsingModule.class,
         FlagsModule.class,
         FlagDependenciesModule.class,

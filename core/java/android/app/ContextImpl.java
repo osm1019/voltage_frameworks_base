b8cca77288d027055ec04fde0e5215a285ce0f34
minaripenguin <minaripenguin@users.noreply.github.com>
2024-02-26 06:53:07 +0800

    core: Gracefully handle unregistering of unregistered receivers/services
    Change-Id: Ia3e7b82389765c3b6d798f4cb4fed3f4b055b880
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -1904,6 +1904,7 @@ class ContextImpl extends Context {
         if (mPackageInfo != null) {
             IIntentReceiver rd = mPackageInfo.forgetReceiverDispatcher(
                     getOuterContext(), receiver);
+            if (rd == null) return;
             try {
                 ActivityManager.getService().unregisterReceiver(rd);
             } catch (RemoteException e) {
@@ -2214,11 +2215,12 @@ class ContextImpl extends Context {
     @Override
     public void unbindService(ServiceConnection conn) {
         if (conn == null) {
-            throw new IllegalArgumentException("connection is null");
+            return;
         }
         if (mPackageInfo != null) {
             IServiceConnection sd = mPackageInfo.forgetServiceDispatcher(
                     getOuterContext(), conn);
+            if (sd == null) return;
             try {
                 ActivityManager.getService().unbindService(sd);
             } catch (RemoteException e) {

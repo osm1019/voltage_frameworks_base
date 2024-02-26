f91175845d79707cc300936c7cd2d13cdb0aa12d
T.J. Mercier <tjmercier@google.com>
2024-02-26 14:05:58 +0000

    Don't skip proess group creation for system_server
    Otherwise system_server incorrectly shows up in the uid_0 (root) cgroup
    in the v2 hierarchy.
    
    Change-Id: I6123b649dabd8c752e527c776d611f5899abfe2c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/jni/com_android_internal_os_Zygote.cpp
+++ core/jni/com_android_internal_os_Zygote.cpp
@@ -1957,7 +1957,7 @@ static void SpecializeCommon(JNIEnv* env, uid_t uid, gid_t gid, jintArray gids, 
 
     // If this zygote isn't root, it won't be able to create a process group,
     // since the directory is owned by root.
-    if (!is_system_server && getuid() == 0) {
+    if (getuid() == 0) {
         const int rc = createProcessGroup(uid, getpid());
         if (rc != 0) {
 #if 0

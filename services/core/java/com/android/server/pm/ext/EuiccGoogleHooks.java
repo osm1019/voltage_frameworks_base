GrapheneOS-Archive/platform_frameworks_base-old

integrate Google's eUICC LPA package (EuiccGoogle)
- isolate EuiccGoogle from all non-system package via AppsFilter, which stops it from sending data
to Google through GmsCore. EuiccGoogle doesn't send data to Google directly
- keep EuiccGoogle disabled by default, but do not disable it after each reboot
- remove a misleading "device information will be sent to Google" message that appears before eSIM
download

services/core/java/com/android/server/pm/ext/EuiccGoogleHooks.java
@@ -1,0 +1,40 @@
+package com.android.server.pm.ext;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+import com.android.server.pm.pkg.PackageStateInternal;
+import com.android.server.pm.pkg.component.ParsedUsesPermission;
+
+class EuiccGoogleHooks extends PackageHooks {
+
+    static class ParsingHooks extends PackageParsingHooks {
+
+        @Override
+        public int overrideDefaultPackageEnabledState() {
+            return PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+        }
+
+        @Override
+        public boolean shouldSkipUsesPermission(ParsedUsesPermission p) {
+            switch (p.getName()) {
+                // Carrier apps aren't shipped on GrapheneOS, these permissions are needed only to
+                // install/enable them
+                case Manifest.permission.INSTALL_EXISTING_PACKAGES:
+                case Manifest.permission.CHANGE_COMPONENT_ENABLED_STATE:
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+    @Override
+    public boolean shouldBlockPackageVisibility(int userId, PackageStateInternal otherPkg) {
+        // Block EuiccGoogle from interacting with GmsCore, which is used for feature flags, logging,
+        // perf data reporting etc.
+        //
+        // Block visibility for the rest of non-system packages to reduce the attack surface.
+        return !otherPkg.isSystem();
+    }
+}

osm1019/voltage_frameworks_base

Signed-off-by: Dmitrii <bankersenator@gmail.com>

services/core/java/com/android/server/pm/ext/EuiccGoogleHooks.java
@@ -1,0 +1,41 @@
+package com.android.server.pm.ext;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+import com.android.internal.pm.pkg.component.ParsedUsesPermission;
+import com.android.internal.pm.pkg.parsing.PackageParsingHooks;
+import com.android.server.pm.pkg.PackageStateInternal;
+
+class EuiccGoogleHooks extends PackageHooks {
+
+    static class ParsingHooks extends PackageParsingHooks {
+
+        @Override
+        public int overrideDefaultPackageEnabledState() {
+            return PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+        }
+
+        @Override
+        public boolean shouldSkipUsesPermission(ParsedUsesPermission p) {
+            switch (p.getName()) {
+                // Carrier apps aren't shipped on GrapheneOS, these permissions are needed only to
+                // install/enable them
+                case Manifest.permission.INSTALL_EXISTING_PACKAGES:
+                case Manifest.permission.CHANGE_COMPONENT_ENABLED_STATE:
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+    @Override
+    public boolean shouldBlockPackageVisibility(int userId, PackageStateInternal otherPkg) {
+        // Block EuiccGoogle from interacting with GmsCore, which is used for feature flags, logging,
+        // perf data reporting etc.
+        //
+        // Block visibility for the rest of non-system packages to reduce the attack surface.
+        return !otherPkg.isSystem();
+    }
+}